e104868cae504e6d7bf2f2e7152d36f6ae3c8ee1
PabloMesquida <pablomesquida67@gmail.com>
2022-12-06 11:21:08 +0100

    slash

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //const URI = process.env.REACT_APP_REDIRECT_URI;
-const URI = "https://beatpoints.vercel.app/";
+const URI = "https://beatpoints.vercel.app";
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=eafd5a23f1cb4f02b98c1cda9aa21333&response_type=code&redirect_uri=${URI}&scope=streaming%20user-read-email%20user-read-playback-state%20user-modify-playback-state`;
 
