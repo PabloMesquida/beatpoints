f402094f523e3160f38e9b4d1e7bee7b179a58be
PabloMesquida <pablomesquida67@gmail.com>
2022-12-06 17:36:13 +0100

    margins

--- src/pages/Dashboard.styles.js
+++ src/pages/Dashboard.styles.js
@@ -1,20 +1,26 @@
 import styled from "styled-components";
 
+let vh = window.innerHeight;
+
 export const DashboardContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  width: 98vw;
+  width: 100vw;
+  height: 100vh;
+  height: ${vh}px;
   border: 1px solid #fff;
 `;
 
 export const UserContainer = styled.div`
   width: 10vw;
+  margin: 0.2rem 0 0.2rem 0.2rem;
   background-color: "#ff0";
 `;
 
 export const DataSongContainer = styled.div`
   width: 30vw;
+  margin: 0.2rem 0;
   background-color: "#f0f";
 `;
 
